84b741946a8cf145ad3b5de9e13727f627974c54
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-01-18 15:47:48 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (91.36%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        
+        int n = s.size();
+        int dp[n+1];
+        if(s[0]=='0')return 0;
+        for(int i=0;i<=n;i++)dp[i]=0;
+        
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            
+            int a = s[i-2]-'0';
+            int b = s[i-1]-'0';
+            
+            int num = a*10+b;
+            
+            if(b!=0)dp[i]+=dp[i-1];
+            if(num>=10 && num<=26)dp[i]+=dp[i-2];
+            
+        }
+        
+        return dp[n];
+        
+        
+    }
+};
