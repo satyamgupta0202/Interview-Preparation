8b85b62639e02985b5849e9885059dd33b45c966
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-01-19 21:32:52 +0530

    Time: 8 ms (68.04%), Space: 7.3 MB (60.98%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -4,21 +4,20 @@ public:
         int n = s.size();
         bool dp[n][n];
         int cnt=0;
-        
+        memset(dp,false,sizeof(dp));
        // for(int i=0;i<n;i++)dp[i][i]=1;
         
         for(int i=n-1;i>=0;i--){
             for(int j=i;j<n;j++){
 
                     if(s[i]==s[j]){
-                        if(j==i ||j-i==1 || dp[i+1][j-1] ){
+                        if(j==i || j-i==1 || dp[i+1][j-1] ){
                             dp[i][j]=true;
-                        }else{
-                            dp[i][j]=false;
                         }
-                    }else{
-                        dp[i][j]=false;
                     }
+                // else{
+                //         dp[i][j]=false;
+                //     }
     
                if(dp[i][j])cnt++; 
             }
