c103114c005a0caa7dcb2d7fc16e5c663f082df1
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-01-21 13:15:17 +0530

    Time: 6 ms (48.50%), Space: 6.7 MB (54.09%) - LeetHub

--- 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-long long  mod = 1e9+7;
-long  solve(string &s) {
+int mod = 1e9+7;
+int solve(string &s) {
 
     unordered_map<char,int>mp;
     for(char ch = 'a' ; ch<='z' ;ch++){
@@ -10,31 +10,23 @@ long  solve(string &s) {
 
     int n = s.size();
     long long dp[n+1];
-
     dp[0]=1;
-    int ok=0;
-
+   
     for(int i=1;i<=n;i++){
 
-        dp[i] = (dp[i-1]*2)%mod;
+        dp[i] = (dp[i-1]*2)%mod;   
         char k = s[i-1];
         if(mp[k]!=-1){
-           ok++;
-            dp[i] = (dp[i] -  dp[mp[k]-1] +mod)%mod;
+            dp[i] = (dp[i] -  dp[mp[k]-1] +mod)%mod;      // substract the matching character -1 count
 
         }
-
         mp[k]=i;
-
-
     }
 
- return dp[n]-1;
-
-
+ return dp[n]-1;  //-1 for non empty sunsequence only
 }
-    long long distinctSubseqII(string s) {
-       return  solve(s);
     
+    int distinctSubseqII(string s) {
+       return  solve(s);
     }
 };
