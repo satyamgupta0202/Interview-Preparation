092213c396a8cb7d1793b95793bfe9a4a7a20a16
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-01-23 13:11:27 +0530

    Time: 784 ms (34.61%), Space: 8.2 MB (69.23%) - LeetHub

--- 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
+++ 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
@@ -9,46 +9,38 @@ public:
 //  0 1
 //  1 0
         
-//         try all combinations : Left-> multiply by 2 , right shift / by 2
+//       try all combinations : Left-> multiply by 2 , right shift / by 2
         int ans = 0;
         bool ok =false;
-        for(int mask = 0 ; mask< 1<<n ; mask++){  //2^n-1 combination of subset 
+        for(int mask = 0 ; mask< 1<<n ; mask++){     //2^n-1 combination of subset 
         
             ok = true;
             
-            for(int i=0;i<n;i++){
+            for(int i=0;i<n;i++){      //check ith bit 
                 
-                if(!( mask>>i & 1  )){
+                if(!( mask>>i & 1  )){   //ith is good whatever hw will say is always correct, so we will check further and if he is not good then we have considered for now he is good and continued further
+                    
                     continue;
                 }
                 
-                for(int j=0;j<n;j++){
+                for(int j=0;j<n;j++){      //now we check jth person if he doestnt make opinion for ith person we will simply continue
                     if(i==j || s[i][j]==2){
                         continue;
                     }
-                    
+//     otherwise we will check the opinions with s[i][j] if they dont match our subset is invalid                
                     if( (mask>>j &1) != s[i][j] ){
                         ok = false;
                     }
                 }
                 
             }
-            
+//             if we get valid subset we will check for makimum 1 
             if(!ok){
                 continue;
             }
             
-            ans = max(ans , __builtin_popcount(mask));
-            
-            
-            
+            ans = max(ans , __builtin_popcount(mask));     
         }
-        
-        
-        
-        return ans;
-        
-        
-        
+        return ans;   
     }
 };
