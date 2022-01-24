fa3d6c41085c3cdf2f55fde732052ec35e814c44
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-01-24 19:20:44 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (34.51%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,21 +11,50 @@
  */
 class Solution {
 public:
-     vector<int>ans;
-    
-    void inorder(TreeNode* root){
-        if(root==NULL){
-            return;
-        }
-        
-        inorder(root->left);
-        
-        ans.push_back(root->val);
-        
-        inorder(root->right);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
-        inorder(root);
-        return ans;
+    using node = TreeNode;
+    vector<int>ans;
+    stack<node*>s;
+    node *temp=root;
+    while(true)
+    {
+        if(temp!=NULL){
+        s.push(temp);
+        temp=temp->left;
+        }
+        else
+        {
+            if(s.size()==0)  break;
+            temp=s.top();
+            s.pop();
+            ans.push_back(temp->val);
+            temp=temp->right;
+        }
     }
+    return ans;        
+    }
+     
 };
+
+
+
+// class Solution {
+// public:
+//      vector<int>ans;
+    
+//     void inorder(TreeNode* root){
+//         if(root==NULL){
+//             return;
+//         }
+        
+//         inorder(root->left);
+        
+//         ans.push_back(root->val);
+        
+//         inorder(root->right);
+//     }
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         inorder(root);
+//         return ans;
+//     }
+// };
