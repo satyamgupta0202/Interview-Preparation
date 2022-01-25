48f653fd965eea1acf0a7b6dd0298d409226e541
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-01-25 20:38:23 +0530

    Time: 50 ms (16.18%), Space: 27.6 MB (91.93%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -12,20 +12,23 @@
 class Solution {
 public:
     
+// Important
+//for every node we determine maximum val in the left and right path it would give to us
+// 
+    
+    
     int ans = INT_MIN;
     
     int height(TreeNode* root){
         
         if(root==NULL)return 0;
-        
         int lh = max(0,height(root->left));
         int rh = max(0,height(root->right));
         
+//         Everytime we keep updating our ans as , lh+rh+root->val
         ans = max(ans , lh+rh+root->val);
-        
         return max(lh,rh)+root->val;
-
-        
+   
     }
     
     int maxPathSum(TreeNode* root) {
