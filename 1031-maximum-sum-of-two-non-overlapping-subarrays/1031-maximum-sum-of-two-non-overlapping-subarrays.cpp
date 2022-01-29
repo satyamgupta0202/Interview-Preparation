2cefe87fec144e626b335cb62f7661c59faaccfd
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-01-29 12:59:22 +0530

    Time: 5 ms (52.70%), Space: 8.5 MB (60.03%) - LeetHub

---
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    int maxSumTwoNoOverlap(vector<int>& a, int f, int s) {
+        int n=a.size(),sum=0;
+		// We are going to create two arrays for left max sum and right max sum
+		/*
+		There are going to be 2 cases;-
+		1. Subarray of length f comes first followed by subarray of length s
+		2. Subarray of length s comes first followed by subarray of length f
+		 and then we will return the maximum sum got from both cases.*/
+		
+		
+        int dp1[n],dp2[n];
+		
+		// Case 1.
+		
+        for(int i=0;i<n;i++)
+        {
+            if(i<f){
+                sum+=a[i];
+                dp1[i]=sum;
+            }
+            else
+            {
+                sum+=a[i]-a[i-f];
+                dp1[i]=max(sum,dp1[i-1]);
+            }
+        }
+        sum=0;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            if(i+s>=n)
+            {
+                sum+=a[i];
+                dp2[i]=sum;
+            }
+            else
+            {
+                sum+=a[i]-a[i+s];
+                dp2[i]=max(sum,dp2[i+1]);
+            }
+        }
+        
+        int ans=INT_MIN;
+        for(int i=f-1;i<n-s;i++)
+        {
+            ans=max(ans,dp1[i]+dp2[i+1]);
+        }
+        //Case 2.
+        sum=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(i<s){
+                sum+=a[i];
+                dp1[i]=sum;
+            }
+            else
+            {
+                sum+=a[i]-a[i-s];
+                dp1[i]=max(sum,dp1[i-1]);
+            }
+        }
+        sum=0;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            if(i+f>=n)
+            {
+                sum+=a[i];
+                dp2[i]=sum;
+            }
+            else
+            {
+                sum+=a[i]-a[i+f];
+                dp2[i]=max(sum,dp2[i+1]);
+            }
+        }
+        
+        
+        for(int i=s-1;i<n-f;i++)
+        {
+            ans=max(ans,dp1[i]+dp2[i+1]);
+        }
+        
+        return ans;
+    }
+};
