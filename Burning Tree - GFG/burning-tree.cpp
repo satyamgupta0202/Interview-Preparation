d7fcc756cebba7db5ab9b1e4c4fe35ac558b88cc
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-01-30 16:54:48 +0530

    Added solution - LeetHub

--- Burning Tree - GFG/burning-tree.cpp
+++ Burning Tree - GFG/burning-tree.cpp
@@ -85,58 +85,56 @@ Node *buildTree(string str) {
 
 class Solution {
  public:
-   int minTime(Node* root, int target) 
-   {
-       queue<Node*> q;
-       q.push(root);
-       unordered_map<Node*,Node*> parent;
-       Node* main=NULL;
-       while(!q.empty())
-       {
-           Node* temp=q.front();
-           q.pop();
-           if(temp->left!=NULL)
-           {
-               parent[temp->left]=temp;
-               q.push(temp->left);
-           }
-           if(temp->right!=NULL)
-           {
-               parent[temp->right]=temp;
-               q.push(temp->right);
-           }
-           if(temp->data==target)
-               main=temp;
-       }
-       queue<pair<Node*,int>> q1;
-       unordered_map<Node*,int> vis;
-       q1.push(make_pair(main,0));
-       parent[root]==NULL;
-       int ans=0;
-       while(!q1.empty())
-       {
-           Node* temp=q1.front().first;
-           int t=q1.front().second;
-           ans=max(t,ans);
-           q1.pop();
-           if(temp->left!=NULL && vis[temp->left]==0)
-           {
-               vis[temp->left]++;
-               q1.push(make_pair(temp->left,t+1));
-           }
-           if(temp->right!=NULL && vis[temp->right]==0)
-           {
-               vis[temp->right]++;
-               q1.push(make_pair(temp->right,t+1));
-           }
-           if(parent[temp]!=NULL && vis[parent[temp]]==0)
-           {
-               q1.push(make_pair(parent[temp],t+1));
-               vis[parent[temp]]++;
-           }
-       }
-       return ans;
-   }
+  int minTime(Node* root, int tgt) {
+    unordered_map <Node*, Node*> parent;
+    unordered_map <Node*, bool> visited;
+    queue <Node*> q;
+    q.push(root);
+    Node *final;
+    
+    while(!q.empty()){
+        int n = q.size();
+        while(n--){
+            Node *tmp = q.front();
+            q.pop();
+            if(tmp->data == tgt)
+                final = tmp;
+            if(tmp->left){
+                q.push(tmp->left);
+                parent[tmp->left] = tmp;
+            }
+            if(tmp->right){
+                q.push(tmp->right);
+                parent[tmp->right] = tmp;
+            }
+        }
+    }
+    
+    int curr_level = 0;
+    q.push(final);
+    visited[final] = true;
+    while(!q.empty()){
+        int n = q.size();
+        while(n--){
+            Node *tmp = q.front();
+            q.pop();
+            if(tmp->left && !visited[tmp->left]){
+                q.push(tmp->left);
+                visited[tmp->left] = true;
+            }
+            if(tmp->right && !visited[tmp->right]){
+                q.push(tmp->right);
+                visited[tmp->right] = true;
+            }
+            if(parent[tmp] && !visited[parent[tmp]]){
+                q.push(parent[tmp]);
+                visited[parent[tmp]] = true;
+            }
+        }
+        curr_level ++;
+    }
+    return curr_level-1;
+}
 };
 
 // { Driver Code Starts.
