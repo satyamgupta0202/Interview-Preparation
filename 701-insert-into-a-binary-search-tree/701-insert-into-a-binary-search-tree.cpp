efababcbb5b202f8651d3f3428e39e25de6bcf21
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-01-31 12:31:10 +0530

    Time: 88 ms (84.73%), Space: 57 MB (50.17%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -1,51 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    
-    void solve(TreeNode* root , int val,TreeNode* temp){
-        
-        if(root==NULL){
-            if(temp->val > val){
-                temp->left = new TreeNode(val);
-                return;
-            }
-            else{
-                temp->right = new TreeNode(val);
-                return ;
-            }
-        }
-        
-        
-        if(root->val > val){
-            temp = root;
-            solve(root->left,val,temp);
-        }else{
-            temp = root;
-            solve(root->right,val,temp);
-        }
-        
-    }
-    
     TreeNode* insertIntoBST(TreeNode* root, int val) {
         
-        if(root==NULL){
-            TreeNode* ans = new TreeNode(val);
-            return ans;
-        }
+        TreeNode* temp = new TreeNode(val);
         TreeNode* ans = root;
-        TreeNode* temp;
-        solve(root,val,temp);
+        if(root == NULL){
+            root = temp;
+            return root;
+        }
+        
+        while(root){
+            
+            if(root->val > val){
+                if(root->left == NULL){
+                    root->left = temp;
+                    break;
+                }
+                root=root->left;
+            }
+            else{
+                if(root->right == NULL){
+                    root->right = temp;
+                    break;
+                }
+                root=root->right;
+            }
+            
+        }
         return ans;
-     
+        
     }
 };
