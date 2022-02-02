00b19f064216db1175f607c6e23f3b04c5e02886
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-02 16:39:04 +0530

    Time: 23 ms (34.41%), Space: 13.1 MB (95.81%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,6 +1,60 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        int row = matrix.size() , col = matrix[0].size();
+        bool isRow = false , isCol=false;
+        
+        for(int i=0;i<row;i++){
+            if(matrix[i][0]==0){
+                isCol = true;
+                break;
+            }
+        }
+        for(int i=0;i<col;i++){
+            if(matrix[0][i]==0){
+                isRow=true;
+                break;
+            }
+        }
+        
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+       
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(isRow){
+            for(int i=0;i<col;i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(isCol){
+            for(int i=0;i<row;i++){
+                matrix[i][0]=0;
+            }
+        }
+        
+        
+    }
+};
+
+
+
+/**
+Day 1 : striver
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
         
         int row = matrix.size();
         int col = matrix[0].size();
@@ -16,7 +70,6 @@ public:
         }
         
         while(!q.empty()){
-            
             auto r = q.front().first;
             auto c = q.front().second;
             q.pop();
@@ -26,7 +79,7 @@ public:
             for(int i=0;i<row;i++){
                 matrix[i][c]=0;
             } 
-        }
-        
+        }  
     }
 };
+**/
