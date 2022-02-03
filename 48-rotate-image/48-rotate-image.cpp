c2570ed39bc83e71962bc6e348087923de0702a6
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-03 11:36:59 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (92.97%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
+    
+//     Transpose lekar , har row ko reverse kardia maine
+    
     void rotate(vector<vector<int>>& matrix) {
-        
         int row = matrix.size() , col = matrix[0].size();
-        
         int i=0,j=0;
         int n = row;
-        while(i<row && j<col){
-            
+        while(i<row && j<col){ 
             int p=i;
-            while(p<n){
-               
+            while(p<n){      
                 swap(matrix[i][p] , matrix[p][j]);
-                p++;
-                
+                p++;       
             }
             i++;
             j++;    
         }
-        
-       
-        
-        for(int i=0;i<n;i++){
-            
-            reverse(matrix[i].begin(),matrix[i].end());
-            
-        }
-        
-       
-        
-        
+        for(int i=0;i<n;i++){  
+            reverse(matrix[i].begin(),matrix[i].end());  
+        } 
     }
 };
