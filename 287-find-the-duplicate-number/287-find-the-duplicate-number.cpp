c16a2e6859813051bfd1850db7ed11652fb46456
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-03 15:45:45 +0530

    Time: 192 ms (34.88%), Space: 61.2 MB (91.24%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -5,16 +5,12 @@ public:
         int n = nums.size();
         
         for(int i=0;i<n;i++){
-            
             nums[nums[i]%n]+=n;
-            
         }
         
         for(int i=0;i<n;i++){
             if(nums[i]>=2*n)return i;
         }
-        
         return 0;
-        
     }
 };
