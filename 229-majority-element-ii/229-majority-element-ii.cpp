aff96cba435e5dae1a6bd577363a6bb6dd1c050e
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-04 15:16:43 +0530

    Time: 16 ms (58.86%), Space: 15.9 MB (51.68%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,6 +1,69 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        
+        int cntx=0,cnty=0,x=0,y=0;
+        int n = nums.size();
+        for(auto it: nums)
+        {
+            if(it==x)
+            {
+                cntx++;
+            }
+            else if(it==y)
+            {
+                cnty++;
+            }
+            else if(cntx==0)
+            {
+                cntx=1;
+                x=it;
+            }
+            else if(cnty==0)
+            {
+                cnty=1;
+                y=it;
+            }
+            else
+            {
+                cntx--;
+                cnty--;
+            }
+        }
+        
+        cntx=0;
+        cnty=0;
+        for(auto it: nums)
+        {
+            if(it==x)
+            {
+                cntx++;
+            }
+           else if(it==y)
+            {
+                cnty++;
+            }
+        }
+        vector<int>ans;
+        if(cntx> n/3)
+        {
+            ans.push_back(x);
+        }
+        if(cnty> n/3)
+        {
+            ans.push_back(y);
+        }
+            
+        return ans;
+        
+    }
+};
+
+
+/**
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
         map<int,int>mp;
         int n = nums.size();
         
@@ -16,3 +79,5 @@ public:
         return ans;
     }
 };
+
+**/
