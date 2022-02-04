cafde2aa390b6887c27d99fb99d83fe46f3f28db
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-04 13:41:22 +0530

    Time: 16 ms (89.09%), Space: 19.7 MB (51.55%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,6 +2,38 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
+//         Voting Algorithm
+        int cnt = 0 ,ans = 0;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if(cnt==0)
+            {
+                ans = nums[i];
+            }
+            if(ans == nums[i])
+            {
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+            
+        }
+        return ans;
+        
+    }
+};
+
+
+
+
+/**
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
         int n = nums.size();
         if(n==1)return nums[0];
         int cnt = 0;
@@ -23,8 +55,9 @@ public:
                   cnt=0;
             } 
         }
-        if(ans>=  floor(n/2))
+        if(ans>=floor(n/2))
         return res;
         return 0;
     }
 };
+**/
