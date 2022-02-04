bc122abf99bc94ce40446eed40142f7308f4019c
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-04 12:23:03 +0530

    Time: 14 ms (5.39%), Space: 9.4 MB (95.59%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -2,37 +2,6 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        
-        int row = matrix.size() , col = matrix[0].size();
-        
-        // make it 1-D array
-        int l = 0 , r = row*col-1;
-        
-        while(l<=r){
-            
-            int mid = l+ (r-l)/2;
-            
-            if(matrix[mid/col][mid%col] < target){
-                l=mid+1;
-            }
-            else if(matrix[mid/col][mid%col] > target){
-                r=mid-1;
-            }
-            else{
-                return true;
-            }             
-        }  
-        return false; 
-        
-    }
-};
-
-
-/**
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
         //naive approach (o) m*n
         
         int row = matrix.size() , col = matrix[0].size();
@@ -47,4 +16,3 @@ public:
         return false;
     }
 };
-**/
