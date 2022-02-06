02a253e65ff0046dca82fc3303d51a6c7f096571
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-06 10:39:23 +0530

    Time: 1382 ms (8.33%), Space: 228.9 MB (8.33%) - LeetHub

--- 2166-design-bitset/2166-design-bitset.cpp
+++ 2166-design-bitset/2166-design-bitset.cpp
@@ -1,4 +1,101 @@
 class Bitset {
+    unordered_set<int> ones , zero;
+    int sz;
+    bool flipped = false;
+public:
+    Bitset(int size) {
+        sz = size;
+        for(int i=0;i<size;i++)
+            zero.insert(i);
+    }
+    
+    void fix(int idx) {
+        if(flipped)
+        {
+            if(zero.count(idx))
+                return;
+            else
+            {
+                zero.insert(idx);
+                ones.erase(ones.find(idx));
+            }
+        }
+        else
+        {
+            if(ones.count(idx))
+                return;
+            ones.insert(idx);
+            zero.erase(zero.find(idx));
+        }
+    }
+    
+    void unfix(int idx) {
+        if(flipped)
+        {
+            if(ones.count(idx))
+                return;
+            ones.insert(idx);
+            zero.erase(zero.find(idx));
+        }
+        else
+        {
+            if(zero.count(idx))
+                return;
+            zero.insert(idx);
+            ones.erase(ones.find(idx));
+        }
+    }
+    
+    void flip() {
+        flipped = flipped ? false : true;
+    }
+    
+    bool all() {
+        if(flipped) return size(ones) == 0;
+        else return size(zero) == 0;
+    }
+    
+    bool one() {
+        if(flipped) return size(zero) > 0;
+        else return size(ones) > 0;
+    }
+    
+    int count() {
+        if(flipped) return size(zero);
+        else return size(ones);
+    }
+    
+    string toString() {
+        
+        string str = "";
+        
+        if(flipped)
+        {
+            for(int i=0;i<sz;i++)
+            {
+                if(zero.count(i))
+                    str += '1';
+                else
+                    str += '0';
+            }
+        }
+        else
+        {
+            for(int i=0;i<sz;i++)
+            {
+                if(zero.count(i))
+                    str += '0';
+                else
+                    str += '1';
+            }
+        }
+        
+        return str;
+    }
+};
+
+/***
+class Bitset {
 public:
     vector<int> v;
     int c;// count of flips
@@ -75,3 +172,16 @@ public:
         return ans;
     }
 };
+**/
+
+/**
+ * Your Bitset object will be instantiated and called as such:
+ * Bitset* obj = new Bitset(size);
+ * obj->fix(idx);
+ * obj->unfix(idx);
+ * obj->flip();
+ * bool param_4 = obj->all();
+ * bool param_5 = obj->one();
+ * int param_6 = obj->count();
+ * string param_7 = obj->toString();
+ */
