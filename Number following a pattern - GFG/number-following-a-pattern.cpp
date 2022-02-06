3d48af94617c09f86bfad263e7b8bfcbcdb718cf
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-06 23:08:18 +0530

    Added solution - LeetHub

---
+++ Number following a pattern - GFG/number-following-a-pattern.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution{   
+public:
+    string printMinNumberForPattern(string S){
+        // code here 
+        string ans = "";
+        stack<int>st;
+        int num =1;
+        int n = S.size();
+        
+        for(int i=0;i<n;i++){
+            
+            if(S[i]=='D'){
+            
+                st.push(num);
+                num++;
+                
+            }
+            
+        else{
+            st.push(num);
+            num++;
+            while(!st.empty()){
+                ans+=to_string(st.top());
+                st.pop();
+            }
+        }
+        
+    }
+    st.push(num);
+    while(!st.empty()){
+        ans+=to_string(st.top());
+        st.pop();
+    }
+    return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.printMinNumberForPattern(S) << endl;
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
