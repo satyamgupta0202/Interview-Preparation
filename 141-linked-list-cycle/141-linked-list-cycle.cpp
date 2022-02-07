3c3089e346b5aea71c21c04c20618586147959d5
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-07 13:33:24 +0530

    Time: 24 ms (16.28%), Space: 8 MB (58.28%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -30,6 +30,23 @@ public:
     }
 };
 
+/**    someone on discuss
+public boolean hasCycle(ListNode head) {
+    if(head==null) return false;
+    ListNode walker = head;
+    ListNode runner = head;
+    while(runner.next!=null && runner.next.next!=null) {
+        walker = walker.next;
+        runner = runner.next.next;
+        if(walker==runner) return true;
+    }
+    return false;
+}
+**/
+
+
+
+
 /**
 //      Naive Approach
         map<ListNode* , bool >mp;
