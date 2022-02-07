6638946480220b187c0579d766efbae2b3a50f04
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-07 11:56:50 +0530

    Time: 80 ms (10.64%), Space: 71.4 MB (49.40%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -8,41 +8,65 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+class Solution {
+    
+    public: 
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* c1 = l1;
+        ListNode* c2 = l2;
+        ListNode* sentinel = new ListNode(0);
+        ListNode* d = sentinel;
+        int sum = 0;
+        while (c1 !=NULL|| c2 != NULL) {
+            sum /= 10;
+            if (c1 != NULL) {
+                sum += c1->val;
+                c1 = c1->next;
+            }
+            if (c2 != NULL) {
+                sum += c2->val;
+                c2 = c2->next;
+            }
+            d->next = new ListNode(sum % 10);
+            d = d->next;
+        }
+        if (sum / 10 == 1)
+            d->next = new ListNode(1);
+        return sentinel->next;
+    }
+};
+
+/** Introducing Extra Space 
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
        
         if(!l1)return l2;
         if(!l2)return l1;
-        
         ListNode* Dummy = new ListNode(0);
         int carry = 0;
         ListNode* head = Dummy;
         
-        while(l1 && l2){
-           
+        while(l1 && l2){ 
             int num = l1->val + l2->val + carry;
             int k = num%10;
-            carry = num/10;
-            
+            carry = num/10;  
             head->next = new ListNode(k);
             head = head->next;
             l1=l1->next;
-            l2=l2->next;
-            
+            l2=l2->next;   
         }
         
-        while(l1){
-            
+        while(l1){  
            int num = l1->val  + carry;
             int k = num%10;
             carry = num/10;
             
             head->next = new ListNode(k);
             head = head->next;
-            l1=l1->next;
-           
-            
+            l1=l1->next;     
         }
            while(l2){
            int num = l2->val  + carry;
@@ -51,9 +75,7 @@ public:
             
             head->next = new ListNode(k);
             head = head->next;
-            l2=l2->next;
-           
-            
+            l2=l2->next; 
         }
         
         if(carry>0){
@@ -65,3 +87,4 @@ public:
         
     }
 };
+**/
