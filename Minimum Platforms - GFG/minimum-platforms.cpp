24ad6ab6401f143b77771be527272464ec4365ae
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-09 11:33:50 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -10,16 +10,57 @@ class Solution{
     public:
     //Function to find the minimum number of platforms required at the
     //railway station such that no train waits.
-    int findPlatform(int arr[], int dep[], int n){
+    int findPlatform(int arr[], int dep[], int n)
+    {
     	
+    	int cnt = 0 , ans = 0;
     	
-    	// Your code here
+    	sort(arr,arr+n);
+    	sort(dep , dep+n);
     	
-    	vector<pair<int,int>>v;
+    	int i=0,j=0;
+    	
+    	while(i<=n-1 && j<=n-1){
+    	    
+    	    if(dep[j]>=arr[i]){
+    	        cnt++;
+    	        i++;
+    	        ans = max(ans,cnt);
+    	    }else{
+    	        cnt--;
+    	        j++;
+    	    }
+    	}
+    	
+    	return ans;
+    	
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** 
+ O(N)Space Complexity 
+  	vector<pair<int,int>>v;
     	for(int i=0;i<n;i++){
     	    v.push_back({arr[i],dep[i]});
     	}
-    	
     	sort(v.begin(),v.end());
         priority_queue<int , vector<int> , greater<int>>q;	
     	int ans = 1;
@@ -34,15 +75,9 @@ class Solution{
     	        ans++;
     	    }
     	    q.push(v[i].second);
- 
     	}
-    
-    	
-    	
     	return ans;
-    	
-    }
-};
+**/
 
 
 // { Driver Code Starts.
