783a21522738ffda4ef63f70fda018150448077f
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-09 13:23:18 +0530

    Added solution - LeetHub

--- Number of Coins - GFG/number-of-coins.cpp
+++ Number of Coins - GFG/number-of-coins.cpp
@@ -6,27 +6,29 @@ using namespace std;
 class Solution{
 
 	public:
-	int minCoins(int coins[], int M, int V) 
-	{ 
+	int minCoins(int coins[], int M, int V) { 
 	    // Your code goes here
+	    
 	    int dp[M+1][V+1];
-	    
-	    for(int i=0;i<=V;i++)dp[0][i]=INT_MAX-1;
-	    for(int i=0;i<=M;i++)dp[i][0]=0;
-	    
+	    for(int i=0;i<=V;i++){
+	        dp[0][i]=10000000;
+	    }
+	   // doubt
+	    for(int i=0;i<=M;i++){
+	        dp[i][0]=0;
+	    }
 	    for(int i=1;i<=M;i++){
 	        for(int j=1;j<=V;j++){
-	            if(coins[i-1]>j){
+	            if(j<coins[i-1]){
 	                dp[i][j] = dp[i-1][j];
 	            }else{
-	                dp[i][j] = min(dp[i-1][j] , 1+dp[i][j-coins[i-1]]);
+	                dp[i][j] = min (dp[i-1][j] , 1+dp[i][j-coins[i-1]]);
 	            }
 	        }
 	    }
-	    if(dp[M][V]==INT_MAX-1)return -1;
+	    if(dp[M][V]==10000000)return -1;
 	    return dp[M][V];
-	} 
-	  
+	}
 };
 
 // { Driver Code Starts.
