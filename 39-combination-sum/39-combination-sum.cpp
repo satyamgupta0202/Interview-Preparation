71ab05da1902c7db23c9c0c34860000b2b53f30a
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-10 22:01:35 +0530

    Time: 8 ms (74.83%), Space: 11 MB (58.83%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,82 +2,29 @@ class Solution {
 public:
     vector<vector<int>>ans;
     
-    void solve(vector<int>&a , int target , int indx , vector<int>&path){
-        
-        if(indx==a.size()){
-            if(target==0)ans.push_back(path);
-            
+    void solve(vector<int>&arr , int target , vector<int>&path, int indx , int sum)
+    {
+        if(sum == target){
+            ans.push_back(path);
             return;
         }
+        if(sum>target)return;
         
-        if(a[indx]<=target){
-            path.push_back(a[indx]);
-            solve(a,target-a[indx],indx,path);
+        for(int i=indx;i<arr.size();i++){
+            sum+=arr[i];
+            path.push_back(arr[i]);
+            solve(arr,target,path,i,sum);
             path.pop_back();
+            sum-=arr[i];
         }
-        
-         solve(a,target,indx+1,path);
         
     }
     
-    vector<vector<int>> combinationSum(vector<int>& a, int target) {
+    
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
         
         vector<int>path;
-        solve(a,target,0,path);
-        
+        solve(arr,target,path,0,0);
         return ans;
-        
     }
 };
-
-
-
-/**
-class Solution {
-public:
-    vector<vector<int>>ans;
-    set<vector<int>>s;
-    void solve(vector<int>&a , int target , vector<int>&path , int indx,int sum){
-        
-        if(indx>=a.size()){
-            int ss=0;
-            
-            for(int i=0;i<path.size();i++){
-               ss+=path[i];
-            }
-          
-            if(ss==target){
-                s.insert(path);
-            }
-
-            return;
-        }
-        if(sum>target)return ;
-        
-        path.push_back(a[indx]);
-        solve(a,target,path,indx,sum+a[indx]);
-        path.pop_back();
-       
-        path.push_back(a[indx]);
-        solve(a,target,path,indx+1,sum+a[indx]);
-        path.pop_back();
-        solve(a,target,path,indx+1,sum);       
-       
-    }
-    
-    
-    
-    vector<vector<int>> combinationSum(vector<int>& a, int target) {
-        
-        vector<int>path;
-        int indx = 0;
-        int sum = 0;
-        solve(a,target,path,0,0);
-        vector<vector<int>>res;
-        for(auto it: s){
-            res.push_back(it);
-        }
-        return res;
-    }
-};
-**/
