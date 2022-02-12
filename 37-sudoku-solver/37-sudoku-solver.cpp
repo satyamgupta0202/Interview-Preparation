b54b9903d407c7c90b5863283f5198f64236d357
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-12 14:54:56 +0530

    Time: 7 ms (94.48%), Space: 6.5 MB (23.21%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,42 +1,70 @@
 class Solution {
 public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
-    }
     
-    bool solve(vector<vector<char>>& board){
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[0].size(); j++){
-                if(board[i][j] == '.'){
-                    for(char c = '1'; c <= '9'; c++){
-                        if(isValid(board, i, j, c)){
-                            board[i][j] = c; 
-                            
-                            if(solve(board))
-                                return true; 
-                            else
-                                board[i][j] = '.'; 
-                        }
-                    }
+bool solve(vector<vector<char>>& board ,  vector<vector<int>>&row ,  vector<vector<int>>&col ,          vector<vector<int>>&box ,int r , int c ){
+        
+        if(r==9 && c == 0)return true;
+    
+        int nr = r , nc = c+1;
+    
+        if(c==9){
+            nr++;
+            nc=0;
+        }
+    
+    
+        if(board[r][c]!='.'){
+            return solve(board, row,col,box,nr,nc);
+        }
+        
+            for(int k=1;k<=9;k++){
+                
+                int kk = (r/3)*3 +(c/3);
+                
+                if( row[r][k]==0 && col[c][k]==0 && box[kk][k] == 0 ){
                     
-                    return false;
-                }
-            }
+                    board[r][c] = k+'0';
+                    row[r][k]=1;
+                    col[c][k]=1;
+                    box[kk][k]=1;
+                    
+                    if(solve(board, row,col,box,nr,nc))return true;
+                    
+                    board[r][c] = '.' ;
+                    row[r][k]=0;
+                    col[c][k]=0;
+                    box[kk][k]=0;        
+                }   
         }
-        return true;
+    return false;
+        
+        
     }
     
-    bool isValid(vector<vector<char>>& board, int row, int col, char c){
-        for(int i = 0; i < 9; i++) {
-            if(board[i][col] == c) 
-                return false; 
-            
-            if(board[row][i] == c) 
-                return false; 
-            
-            if(board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) 
-                return false; 
-        }
-        return true;
+    
+    
+    void solveSudoku(vector<vector<char>>& board) {
+        
+        vector<vector<int>>row(9,vector<int>(10,0));
+        vector<vector<int>>col(9,vector<int>(10,0));
+        vector<vector<int>>box(9,vector<int>(10,0));
+
+
+        
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                
+                if(board[i][j]!='.'){
+                    int val = board[i][j] - '0';
+                   // cout<<val<<endl;
+                    row[i][val]=1;
+                    col[j][val]=1;
+                    int temp = (i/3)*3 + (j/3);
+                    box[temp][val]=1;  
+                }
+                   
+            }
+        } 
+        solve(board, row, col , box , 0,0); 
     }
 };
