9066fc736487f4855c231e04587b6281879d8ac6
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-12 10:39:54 +0530

    Time: 5 ms (39.56%), Space: 8 MB (29.09%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
+    
     vector<vector<int>>ans;
     
-    void solve(vector<int>&nums,map<int,bool>&mp , vector<int>&path ,  int i , int n){
+    void solve(vector<int>&nums , int n , vector<int>&path , map<int,int>&mp , int ind){
         
         if(path.size()==n){
             ans.push_back(path);
             return;
         }
         
+        if(ind>=n)return;
+        
         for(int i=0;i<n;i++){
-            if(!mp[nums[i]]){
-                path.push_back(nums[i]);
-                mp[nums[i]]=true;
-                solve(nums,mp,path,i,n);
-                path.pop_back();
-                mp[nums[i]]=false;
-            }
             
-        }
+            if(mp[nums[i]]==-1){
+                
+                path.push_back(nums[i]);
+                mp[nums[i]]=1;
+                solve(nums,n,path,mp,i);
+                path.pop_back();
+                mp[nums[i]]=-1; 
+                //solve(nums,n,path,mp,i+1);
+          }
+            
+           
+    }
+        
+        
+       
+        
+    
+            
+ 
     }
     
-    
     vector<vector<int>> permute(vector<int>& nums) {
+        
         int n = nums.size();
-        map<int,bool>mp;
-        for(int i=0;i<n;i++)mp[nums[i]]=false;
         vector<int>path;
-        solve(nums,mp,path,0,n);
+        
+        map<int,int>mp;
+        for(int i=0;i<n;i++)mp[nums[i]]=-1;
+        
+        solve(nums,n,path,mp,0);
         return ans;
     }
 };
