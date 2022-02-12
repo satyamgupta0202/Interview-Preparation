00bcd867a1534725ea31ea05208a0313c9a12d71
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-12 11:01:53 +0530

    Time: 9 ms (19.32%), Space: 7.8 MB (56.97%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -3,9 +3,49 @@ public:
     
     vector<vector<int>>ans;
     
-    void solve(vector<int>&nums , int n , vector<int>&path , map<int,int>&mp , int ind){
+    void solve(vector<int>&nums , int n , vector<int>&path , int ind){
         
         if(path.size()==n){
+            ans.push_back(path);
+            return ;
+        }
+        
+        if(ind>=n)return;
+        
+        for(int i=ind;i<n;i++){
+            
+            path.push_back(nums[i]);
+            swap(nums[ind],nums[i]);
+            solve(nums,n,path,ind+1);
+            swap(nums[ind],nums[i]);
+            path.pop_back();
+           
+            
+            
+            
+            
+        }
+        
+       
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int>path;
+        
+        // map<int,int>mp;
+        // for(int i=0;i<n;i++)mp[nums[i]]=-1;
+        
+        solve(nums,n,path,0);
+        return ans;
+    }
+};
+
+
+
+/** extra space 
+ if(path.size()==n){
             ans.push_back(path);
             return;
         }
@@ -22,28 +62,6 @@ public:
                 path.pop_back();
                 mp[nums[i]]=-1; 
                 //solve(nums,n,path,mp,i+1);
-          }
-            
-           
-    }
-        
-        
-       
-        
-    
-            
- 
-    }
-    
-    vector<vector<int>> permute(vector<int>& nums) {
-        
-        int n = nums.size();
-        vector<int>path;
-        
-        map<int,int>mp;
-        for(int i=0;i<n;i++)mp[nums[i]]=-1;
-        
-        solve(nums,n,path,mp,0);
-        return ans;
-    }
-};
+          }    
+       }
+       **/
