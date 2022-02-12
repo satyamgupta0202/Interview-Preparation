9949ca460272d9003ab33e86f3c6e7e6c7836174
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-12 11:40:02 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (67.77%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -14,19 +14,14 @@ public:
             solve(v,i+1,n,path,ans);
             path.pop_back();  
            // solve(v,i+1,n,path,ans);
-        }
-        
-        // path.push_back(v[indx]);
-        // solve(v,indx+1,n,path,ans);
-        // path.pop_back();
-        // solve(v,indx+1,n,path,ans);
-        
+        }     
     }
     
     
     
     vector<vector<int>> subsets(vector<int>& num1) {
         
+        sort(num1.begin(),num1.end());
         vector<vector<int>>ans;
         vector<int>path;
         int n = num1.size();
