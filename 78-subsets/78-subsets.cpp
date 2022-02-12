8841019c0807f544033b05a4e6b613becbeebb06
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-12 11:39:13 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (53.30%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,24 +3,23 @@ public:
     
     void solve(vector<int>v , int indx , int n , vector<int>&path , vector<vector<int>>&ans){
         
-        if(indx == n){
-            ans.push_back(path);
-            return ;   
+       ans.push_back(path);
+        
+        if(indx>=n)return;
+        
+        for(int i=indx;i<n;i++){
+            if(i!=indx && v[i]==v[i-1])continue;
+            
+            path.push_back(v[i]);
+            solve(v,i+1,n,path,ans);
+            path.pop_back();  
+           // solve(v,i+1,n,path,ans);
         }
         
-//         for(int i=indx;i<n;i++){
-//            // if(i!=indx && v[i]==v[i-1])continue;
-            
-//             path.push_back(v[i]);
-//             solve(v,i+1,n,path,ans);
-//             path.pop_back();  
-//            // solve(v,i+1,n,path,ans);
-//         }
-        
-        path.push_back(v[indx]);
-        solve(v,indx+1,n,path,ans);
-        path.pop_back();
-        solve(v,indx+1,n,path,ans);
+        // path.push_back(v[indx]);
+        // solve(v,indx+1,n,path,ans);
+        // path.pop_back();
+        // solve(v,indx+1,n,path,ans);
         
     }
     
