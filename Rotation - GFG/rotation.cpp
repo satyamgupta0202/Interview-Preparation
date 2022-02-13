976533ba3b74e10ceae359957555b79ef44fa368
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-13 15:15:51 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.cpp
+++ Rotation - GFG/rotation.cpp
@@ -15,24 +15,17 @@ public:
 	    while(low<=high)
 	    {
 	        int mid = low + (high-low)/2;
-	        
             if(mid+1<n && arr[mid]>arr[mid+1])return mid+1;
-            
             else if(mid-1>=0 && arr[mid]<arr[mid-1])return mid;
-
-	        
-	        
 	        else if(arr[mid]<arr[low]){
 	            high=mid-1;
 	        }
 	        else{
 	            low=mid+1;
 	        }
-	        
 	    }
 	    return 0;
 	}
-
 };
 
 // { Driver Code Starts.
