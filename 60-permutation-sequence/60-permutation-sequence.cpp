199ac3dafa13036a5fffcd653ef8341b2d09b358
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-13 10:35:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.76%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,5 +1,63 @@
 class Solution {
 public:
+    
+
+   int fact[12];
+    
+    void fun()
+    {
+        fact[0]=1;
+        fact[1]=1;
+        for(int i=2;i<=11;i++)
+        {
+            fact[i] = i*fact[i-1];
+         }        
+     }
+
+    
+    
+    
+    
+    string getPermutation(int n, int k) {
+        
+        
+        
+        
+        fun();
+        
+        
+        string temp ="";
+        for(int i=1;i<=n;i++){
+            temp.append(to_string(i));
+        }
+        
+        string ans = "";
+        k--;
+        for(int i=1;i<=n;i++)
+        {
+            int index = (k/fact[n-i]);
+            
+            ans.push_back(temp[index]);
+            
+            
+             temp.erase(temp.begin()+index);
+             cout<<temp<<endl;
+             k=k-(index)*fact[n-i];
+            
+        }
+        
+        return ans;
+        
+        
+    }
+};
+
+
+
+
+/** Method 1:
+class Solution {
+public:
     string getPermutation(int n, int k) {
         
         string temp = "";
@@ -16,3 +74,4 @@ public:
         
     }
 };
+**/
