a5111faf88bb0c85c7426d853946c0245d5324fc
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-21 19:25:08 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -13,6 +13,54 @@ class Solution
     //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& arr, int n) 
     {
+        
+        stack<int>st;
+        for(int i=0;i<n;i++)
+        {
+            st.push(i);
+        }
+        
+        while(st.size()!=1)
+        {
+            int i = st.top();
+            st.pop();
+            int j = st.top();
+            st.pop();
+            
+            if(arr[i][j]==1)
+            {
+                st.push(j);
+            }
+            else
+            {
+                st.push(i);
+            }
+            
+        }
+        
+        int pot = st.top();
+        
+        for(int i=0;i<n;i++)
+        {
+            if(i!=pot)
+            {
+                if(arr[pot][i]==1 || arr[i][pot]==0)
+                {
+                    return -1;
+                }
+            }
+        }
+        return pot;
+    }
+};
+
+/**
+ * class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& arr, int n) 
+    {
         // code here 
         vector<int>in(n,0);
         vector<int>out(n,0);
@@ -37,6 +85,7 @@ class Solution
         return -1;
     }
 };
+**/
 
 // { Driver Code Starts.
 
