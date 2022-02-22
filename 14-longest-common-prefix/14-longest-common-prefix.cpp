7ef9d31d5867dfe82afa0075f2a75b60e374e386
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-22 18:10:41 +0530

    Time: 3 ms (85.76%), Space: 9.2 MB (44.62%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& str) {
+        
+        string ans  = "";
+        int n = str.size();
+        string temp = str[0];
+        
+        for(int i=0;i<temp.size();i++)
+        {
+            char k = temp[i];
+            cout<<k<<endl;
+            int c=0;
+            bool ok = true;
+            for(int j=1;j<n;j++)
+            {
+                if(str[j][i]==k)c++;
+                else ok = false;
+            }
+            
+            if(c==n-1 && ok){
+                cout<<c<<endl;
+                ans.push_back(k);
+            }else{
+                return ans;
+            }
+                    
+        }
+        return ans;
+
+    }
+};
