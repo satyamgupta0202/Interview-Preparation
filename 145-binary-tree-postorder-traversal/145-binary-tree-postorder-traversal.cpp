a1fff5c992995cb703524d98eb6489561de86b15
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-02-28 17:47:51 +0530

    Time: 7 ms (12.24%), Space: 8.3 MB (96.46%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -12,32 +12,51 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        
-//         using two stack 
-        vector<int>ans;
-        stack<TreeNode*>s1,s2;
-        if(root==NULL)return ans;
-        
-        s2.push(root);
-        
-        while(!s2.empty()){
-            auto curr = s2.top();
-            s2.pop();
-            s1.push(curr);
-            if(curr->left){
-                s2.push(curr->left);
-            }
-            if(curr->right){
-                s2.push(curr->right);
-            } 
+        vector<int>res;
+        if(root==NULL)
+            return res;
+        stack<TreeNode*> st1;
+        st1.push(root);
+        while(!st1.empty()){
+            root=st1.top();
+            st1.pop();
+            res.push_back(root->val);
+            if(root->left)
+                st1.push(root->left);
+            if(root->right)
+                st1.push(root->right);
         }
-        while(!s1.empty()){
-            ans.push_back(s1.top()->val);
-            s1.pop();
-        }
-        return ans;
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
+//     vector<int> postorderTraversal(TreeNode* root) {
+        
+// //         using two stack 
+//         vector<int>ans;
+//         stack<TreeNode*>s1,s2;
+//         if(root==NULL)return ans;
+        
+//         s2.push(root);
+        
+//         while(!s2.empty()){
+//             auto curr = s2.top();
+//             s2.pop();
+//             s1.push(curr);
+//             if(curr->left){
+//                 s2.push(curr->left);
+//             }
+//             if(curr->right){
+//                 s2.push(curr->right);
+//             } 
+//         }
+//         while(!s1.empty()){
+//             ans.push_back(s1.top()->val);
+//             s1.pop();
+//         }
+//         return ans;
+//     }
+// };
 
 // Ierative approach
 // vector<int>ans;
