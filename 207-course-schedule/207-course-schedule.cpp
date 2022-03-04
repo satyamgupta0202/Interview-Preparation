a124346ea384c8dd2a6615d01d1f2bb7d254ad59
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-03-04 17:06:10 +0530

    Time: 12 ms (99.61%), Space: 13.7 MB (70.01%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,64 +1,45 @@
 class Solution {
 public:
     
-    bool cycle(int node,vector<int>adj[],vector<int>&vis,vector<int>&dfsVis )
-    {
+    bool dfs(int node ,vector<int>&vis , vector<int>&dfsVis , vector<int>adj[] ){
+        
         vis[node]=1;
         dfsVis[node]=1;
         
-        for(auto it :  adj[node])
-        {
-            if(vis[it]==0)
-            {
-                if(cycle(it,adj,vis,dfsVis) == true)
-                {
-                    return true;
-                }
+        for(auto it: adj[node]){
+            if(vis[it]==0){
+                if(dfs(it,vis,dfsVis,adj))return true;
             }
-            else if(dfsVis[it]==1)return true;
+            else if(dfsVis[it]==1){
+                return true;
+            }
         }
-        
         dfsVis[node]=0;
         return false;
+        
     }
     
     
-    bool canFinish(int n, vector<vector<int>>& v) 
-    {
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         
-        vector<int>adj[n];
-        vector<int>vis(n,0);
-        vector<int>dfsVis(n,0);
+        int n = prerequisites.size();
+        vector<int>adj[numCourses];
+        vector<int>vis(numCourses,0);
+        vector<int>dfsVis(numCourses,0);
         
-        for(int i=0;i<v.size();i++)
-        {
-            int a = v[i][0];
-            int b = v[i][1];
-            adj[b].push_back(a);
+        for(int i=0;i<n;i++){
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+            adj[a].push_back(b);
         }
         
-        
-        
-        for(int i=0;i<n;i++)
-        {
-            if(vis[i]==0)
-            {
-                if(cycle(i,adj,vis,dfsVis) == true)
-                {
-                    return false;
-                }
+        for(int i=0;i<numCourses;i++){
+            if(vis[i]==0){
+               if(dfs(i,vis,dfsVis,adj))return false;
             }
         }
-        if(cycle(0,adj,vis,dfsVis) == true)return false;
+        return true;
         
-//         for(int i=0;i<v.size();i++)
-//         {
-//             int a = v[i][0];
-//             if(cycle(a,adj,vis,dfsVis)==true)return false;
-//         }
-        
-        
-        
-         return true;
     }
 };
