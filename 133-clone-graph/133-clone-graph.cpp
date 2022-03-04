2132305d2d5343a4a63edfb334d1c85ee12b9437
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-03-04 21:40:34 +0530

    Time: 7 ms (73.90%), Space: 9.2 MB (7.13%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -1,68 +1,31 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
-public:
-    
-    void dfs(Node* curr , Node* node , vector<Node*>&vis )
+    public:
+    Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
     {
-        vis[node->val] = node;
-        
-        for(auto it: curr->neighbors)
-        {
-            if(vis[it->val]==NULL){
-            Node* nn = new Node(it->val);
-            node->neighbors.push_back(nn);
-            dfs(it,nn,vis);
+        vector<Node*> neighbour;
+        Node* clone=new Node(cur->val);
+        mp[cur]=clone;
+            for(auto it:cur->neighbors)
+            {
+                if(mp.find(it)!=mp.end())   //already clone and stored in map
+                {
+                    neighbour.push_back(mp[it]);    //directly push back the clone node from map to neigh
+                }
+                else
+                    neighbour.push_back(dfs(it,mp));
             }
-            else{
-                node->neighbors.push_back(vis[it->val]);
-            }
-        }
-        
+            clone->neighbors=neighbour;
+            return clone;
     }
-    
-    Node* cloneGraph(Node* node) 
-    {
-        
-        if(node == NULL)return NULL;  //base
-        
-        
-        vector<Node*>vis(1000,NULL);
-        Node* copy = new Node(node->val);
-        vis[node->val] = copy;
-        
-        for(auto it : node->neighbors)
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*,Node*> mp;
+        if(node==NULL)
+            return NULL;
+        if(node->neighbors.size()==0)   //if only one node present no neighbors
         {
-            if(vis[it->val]==NULL)
-            {
-                Node* nn = new Node(it->val);
-                (copy->neighbors).push_back(nn);
-                dfs(it,nn,vis);
-            }
-            else
-            {
-                (copy->neighbors).push_back(vis[it->val]);
-            }
+            Node* clone= new Node(node->val);
+            return clone; 
         }
-        return copy;   
+        return dfs(node,mp);
     }
 };
