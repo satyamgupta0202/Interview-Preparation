0d18c6142c7836dc53b733922fa05bcffc73a4f3
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-03-11 19:40:48 +0530

    Time: 4 ms (70.50%), Space: 7.8 MB (57.18%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    
-    void solve(vector<int>& nums, int indx , vector<int>&path){
-      
-       
+    //Cisco
+    void solve(vector<int>& nums, int indx , vector<int>&path)
+    {
         ans.push_back(path);
         
-
-        
-        for(int i=indx;i<nums.size();i++){
+        for(int i=indx;i<nums.size();i++)
+        {
             
-            if(i!=indx && nums[i]==nums[i-1]){
+            if(i!=indx && nums[i]==nums[i-1])
+            {
                 continue;
             }
-            
             path.push_back(nums[i]);
             solve(nums,i+1,path);
             path.pop_back();
