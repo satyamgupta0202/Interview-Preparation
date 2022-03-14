4bdfd3bfc14df7f90a5cd4796a5a653128547df5
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-03-15 00:36:05 +0530

    Time: 276 ms (41.04%), Space: 7.4 MB (88.47%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,30 +1,42 @@
-int dfs(vector<vector<int>>& grid, int i,int j)
-{
-    
-    int ans=0, temp=0;
-    if(i>=grid.size() || i<0 || j<0 || j>=grid[0].size() || grid[i][j]==0) return 0;
-    temp=grid[i][j];
-    grid[i][j]=0;
-    ans = max(ans,dfs(grid,i-1,j));
-    ans = max(ans,dfs(grid,i+1,j));
-    ans = max(ans,dfs(grid,i,j+1));
-    ans = max(ans,dfs(grid,i,j-1));
-    grid[i][j] = temp;
-    return ans+temp;
-}
-
 class Solution {
 public:
-    int getMaximumGold(vector<vector<int>>& grid){
+    
+    int ans = 0;
+    
+    
+    int dfs(vector<vector<int>>& grid,int i , int j ,int r , int c , int s)
+    {
+        if(i<0 || j<0 || i>=r || j>=c || grid[i][j]==0 )return 0;
         
-        int ans=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]>0){
-                    ans = max(ans,dfs(grid,i,j));
+            int temp = grid[i][j];
+            grid[i][j]=0;
+            int t1 =  dfs(grid,i+1,j,r,c,s);
+            int t2 =  dfs(grid,i-1,j,r,c,s);
+            int t3 =  dfs(grid,i,j-1,r,c,s);
+            int t4 =  dfs(grid,i,j+1,r,c,s); 
+            grid[i][j]=temp;
+            return grid[i][j] + max(max(t1,t2) , max(t3,t4));  
+    }
+    
+    int getMaximumGold(vector<vector<int>>& grid) 
+    {
+        int r = grid.size();
+        int c = grid[0].size();
+        
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(grid[i][j]!=0)
+                {
+                    int s = 0;
+                    
+                    ans = max(ans , dfs(grid,i,j,r,c,0));
+                  //  cout<<ans<<endl;
+                    
                 }
             }
-        }
+        }     
         return ans;
     }
 };
