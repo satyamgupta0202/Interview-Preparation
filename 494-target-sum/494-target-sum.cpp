f271ba0157cf537be10c5502f3fa99a1ebca50ee
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-03-15 12:30:31 +0530

    Time: 406 ms (29.16%), Space: 61.1 MB (13.56%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     
+    
+    
      int findTargetSumWays(vector<int>& nums, int S) {
+        
+        //negative sum bhi ho sake isiliye map use kia
         vector<unordered_map<int,int>> mem(nums.size());
         return find(0,nums,S,mem);    
+         
     }
     int find(int p, vector<int>& nums, int sum, vector<unordered_map<int,int>>& mem) {
+        
         if(p==nums.size()) return sum==0;
+        
         auto it = mem[p].find(sum);
         if(it != mem[p].end()) return it->second;
+        
         return mem[p][sum]=find(p+1,nums,sum+nums[p],mem)+find(p+1,nums,sum-nums[p],mem);
     }
 };
