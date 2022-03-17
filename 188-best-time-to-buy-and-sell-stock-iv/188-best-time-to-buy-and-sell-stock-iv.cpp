852a860d6db81328721cc50f6e4f0151b3701b47
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-03-17 14:51:17 +0530

    Time: 428 ms (14.83%), Space: 11.2 MB (66.03%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int maxProfit(int k, vector<int>&a) 
+    int maxProfit(int k, vector<int>& prices) 
     {
-        int n = a.size();
+        int n = prices.size();
         if(n==0)return 0;
         int dp[k+1][n];
         memset(dp,0,sizeof(dp));
-
-       for(int i=1;i<=k;i++){
-        for(int j=1;j<n;j++){
-            
-           int m = dp[i][j-1];
-
-           for(int k=0;k<j;k++){
-               int aa = dp[i-1][k];
-               int bb = a[j]-a[k];
-               m = max(m , aa+bb);
-           }
-
-           dp[i][j] = m;
+        
+        for(int i=1;i<=k;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                int temp = dp[i][j-1];
+                for(int p=0;p<j;p++)
+                {
+                    
+                    int a = dp[i-1][p];
+                    int b = prices[j]-prices[p];
+                    temp = max(temp , a+b);
+                }
+                dp[i][j]=temp;
+            }
         }
-    }
-        
         return dp[k][n-1];
-        
-        
     }
 };
