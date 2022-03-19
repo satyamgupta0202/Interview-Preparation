b6d693de4a16b5d62c3f49046d029837466bc508
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-03-19 13:16:23 +0530

    Time: 20 ms (72.04%), Space: 10.7 MB (14.67%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -3,6 +3,48 @@ public:
     int lengthOfLIS(vector<int>& nums) 
     {
         int n = nums.size();
+        
+        vector<int>dp(n+1,INT_MAX);
+        dp[0]=INT_MIN;
+        
+        for(int i=0;i<n;i++)
+        {
+            
+            auto it = upper_bound(dp.begin(),dp.end() , nums[i]);
+            
+            auto k = it;
+            
+            k--;
+            
+            if(*k != nums[i])
+            {
+                *it = nums[i];
+            }
+        }
+        
+          for(int i=n;i>0;i--)
+        {
+            if(dp[i]!=INT_MAX)
+            {
+                return i;
+            }
+        }
+        
+        return -1;
+            
+        
+    }
+};
+
+
+//o(N^2)
+/**
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) 
+    {
+        // (o(N^2))
+        int n = nums.size();
         int dp[n];
         memset(dp,0,sizeof(dp));
         
@@ -23,3 +65,4 @@ public:
         return *max_element(dp,dp+n);
     }
 };
+**/
