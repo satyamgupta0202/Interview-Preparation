1f2f7e96058ddc12b55419ee8b2e68ed68f0faa1
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-03-22 19:34:31 +0530

    Time: 398 ms (30.34%), Space: 30.2 MB (33.17%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,56 +1,32 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-      
-         int n = s.size();
-         vector<vector<bool>>dp(n,vector<bool>(n,false));
-         int l = 0;
-         int p=-1,q=-1;
-         int ans=0;
+    string longestPalindrome(string s)
+    {
+        
+        int n = s.size();
+        vector<vector<bool>>dp(n , vector<bool>(n,false));
+        int p=-1,q=-1;
+        int l=0;
         
         for(int i=n-1;i>=0;i--)
         {
             for(int j=i;j<n;j++)
             {
-                if(i==j)
+                if(s[i]==s[j])
                 {
-                    dp[i][j]=true;
-                }
-               else if(j-i==1 )
-                {
-                    if(s[i]==s[j])
+                    if(i==j || j-i==1 || dp[i+1][j-1])
                     {
                         dp[i][j]=true;
-                    }
-                    else
-                    {
-                        dp[i][j]=false;
-                    }
-                }
-                else 
-                {
-                    if(s[j]==s[i] && dp[i+1][j-1])
-                    {
-                        dp[i][j]=true;
-                    }
-                    else
-                    {
-                        dp[i][j]=false;
-                    }
-                }
-                
-                if(dp[i][j])
-                {
-                    if(ans<=(j-i+1))
-                    {
-                        p=i;
-                        q=j;
-                        ans = j-i+1;
+                        if(l<(j-i+1))
+                        {
+                            l=j-i+1;
+                            p=i;
+                            q=j;
+                        }
                     }
                 }
             }
         }
-        return s.substr(p,q-p+1);
-        
+        return s.substr(p,q-p+1);      
     }
 };
