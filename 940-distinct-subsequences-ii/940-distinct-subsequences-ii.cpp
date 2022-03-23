40218952d3fe33fe5d59cc1c76ee5c6f92ab34c8
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-03-23 10:07:30 +0530

    Time: 15 ms (21.37%), Space: 6.6 MB (63.48%) - LeetHub

--- 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-int mod = 1e9+7;
-int solve(string &s) {
-
-    unordered_map<char,int>mp;
-    for(char ch = 'a' ; ch<='z' ;ch++){
-        mp[ch] = -1;
-    }
-
-    int n = s.size();
-    long long dp[n+1];
-    dp[0]=1;
-   
-    for(int i=1;i<=n;i++){
-
-        dp[i] = (dp[i-1]*2)%mod;   
-        char k = s[i-1];
-        if(mp[k]!=-1){
-            dp[i] = (dp[i] -  dp[mp[k]-1] +mod)%mod;      // substract the matching character -1 count
-
+    int distinctSubseqII(string s) 
+    {
+        long long mod = 1e9+7;
+        int n = s.size();
+        map<char,int>mp;
+        
+        int dp[n+1];
+        memset(dp,0,sizeof(dp));
+        
+        dp[0]=1;
+        
+        for(int i=1;i<=n;i++)
+        {
+            dp[i] = (dp[i-1]*2)%mod;
+            
+            if(mp.count(s[i-1])!=0)
+            {
+                int k = mp[s[i-1]];
+                
+                dp[i] = (dp[i]%mod - dp[k]%mod + mod)%mod;
+            }
+            
+            mp[s[i-1]]=i-1;
+            
         }
-        mp[k]=i;
-    }
-
- return dp[n]-1;  //-1 for non empty sunsequence only
-}
-    
-    int distinctSubseqII(string s) {
-       return  solve(s);
+        
+        return dp[n]-1;
     }
 };
