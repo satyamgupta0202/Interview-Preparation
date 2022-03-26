2d854d9f75de2944583e6dab221d346acb5ea8b0
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-03-26 09:10:46 +0530

    Time: 270 ms (87.39%), Space: 45.3 MB (84.96%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -3,51 +3,49 @@ public:
     
     int ans = 0;
     void merge(vector<int>&nums, int low , int mid , int high){
-        
         int i=low , j=mid+1;
         
-        while(i<=mid){
-            
+        while(i<=mid)
+          {
             while(j<=high && nums[i]> (long)2*nums[j]){
                 j++;
-            }
+            } 
             
             ans+= j-(mid+1);
-            i++;
-            
-        }
-        
-        
-        
-        
+            i++;     
+         }
         
         int temp[high-low+1];
         i = low ;
         j=mid+1;
         int k=0;
         
-        while(i<=mid && j<=high){
-            
-            if(nums[i]<nums[j]){
+        while(i<=mid && j<=high)
+        {
+            if(nums[i]<nums[j])
+            {
                 temp[k++]=nums[i++];
-            }else{
+            }
+            else
+            {
                 temp[k++]=nums[j++];
             }
         }
-        while(i<=mid){
+        while(i<=mid)
+        {
             temp[k++]=nums[i++];
         }
-        while(j<=high){
+        while(j<=high)
+        {
             temp[k++]=nums[j++];
         }
 
-        for(int i=0;i<high-low+1;i++){
+        for(int i=0;i<high-low+1;i++)
+        {
             nums[low+i]=temp[i];
         }
     }
-    
-    
-    
+ 
     void mergeSort(vector<int>&nums, int low , int high){
         
         if(low>=high)return ;
@@ -63,10 +61,6 @@ public:
         
         int n = nums.size();
         mergeSort(nums,0,n-1);
-        //   for(int i=0;i<nums.size();i++){
-        //     cout<<nums[i];
-        //     cout<<"\n";
-        // }
         return ans;
     }
 };
