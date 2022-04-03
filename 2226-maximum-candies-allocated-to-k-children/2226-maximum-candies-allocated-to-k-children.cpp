c5d9dcc8ceeab5e5e88af7f1aab6487a5edabd48
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-04-03 10:44:15 +0530

    Time: 269 ms (100.00%), Space: 84.4 MB (60.00%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     
-    long long solve(vector<int>&c , long long tof)
-    {
-        // if(tof==0)return 0;
-        long long p=0;
-        
-        for(auto &val : c)
-        {
-            long long x = val;
-            p += x/tof;
-        }
-            
-        
-        return p;
-    }
-    
-    
     int maximumCandies(vector<int>& c, long long k) 
     {
         long long low = 1 , high = 1e9;
@@ -31,17 +15,18 @@ public:
         while(low<=high)
         {
             long long mid = low + ((high-low)/2);
-            long long p = solve(c,mid);
             
-            // cout << mid << "\n";
-            if(mid == 1) cout << p << "\n";
-            
-            // cout << p << "\n";
+            long long p=0;
+        
+            for(auto &val : c)
+            {
+                long long x = val;
+                p += x/mid;
+            }
             
             
             if(p>=k)
             {
-                // cout << low << " " << high << " " << mid << "\n";
                 low=mid+1;
                 ans=max(ans,mid);
             }
