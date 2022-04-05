fba344a07ef83f9bc26a921828eb5c26d24c5768
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-04-05 20:33:24 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.90%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,47 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    int findTheWinner(int n, int k)
-    {
-        if(k==1)return n;
-        ListNode* node = new ListNode(1);
-        ListNode* head = node;
-        for(int i=2;i<=n;i++)
-        {
-            node->next = new ListNode(i);
-            node = node->next;
-        }
-        
-        ListNode* temp = head;
-        node->next = head;
-        ListNode* prev = head;
     
-        while(temp->next != temp)
-        {
-            int cnt = 0;
-            while(cnt!=k-1)
-            {
-                prev = temp;
-                temp=temp->next;
-                cnt++;
-            }
-            
-
-            
-               temp = temp->next;
-               prev->next = temp;      
-   
-        }
-        return temp->val;
+    int solve(int n , int k)
+    {
+        if(n==0)
+            return 1;
         
+        return (solve(n-1,k)+k)%n;
+    }
+    
+    
+    int findTheWinner(int n, int k) {
+        return solve(n,k)+1;
     }
 };
