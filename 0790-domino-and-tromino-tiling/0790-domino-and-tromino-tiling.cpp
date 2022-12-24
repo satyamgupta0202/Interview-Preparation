c6abc69f47c1c9186b572b622fca99582ce474ae
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2022-12-24 11:38:16 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.59%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     int numTilings(int n) {
-        if(n == 1)
-            return 1;
-        if(n == 2)
-            return 2;
-        if(n == 3)
-            return 5;
-        //a = n(1), b = n(2), c = n(3)
-        long res, a = 1, b = 2, c = 5;
-        while(n-- > 3) {
-            res = (2*c + a) % int (1e9 + 7);
-            a = b;
-            b = c;
-            c = res;
+        
+        int dp[n+1];
+        int mod = 1e9+7;
+        for(int i=0;i<=n;i++)dp[i]=0;
+        
+        if(n<=2)return n;
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        if(n==3)return 5;
+        dp[3]=5;
+        for(int i=4;i<=n;i++)
+        {
+            
+            dp[i] = ((2*dp[i-1])%mod + (dp[i-3]%mod))%mod;
+          
+
         }
-        return res;
+        return dp[n];
+        
+        
+        
+        
+        
+        
     }
 };
